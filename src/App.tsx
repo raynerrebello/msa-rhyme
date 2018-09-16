b57cffde381a2dd623cfcac4897fe908eac4098f
rayner rebello <rreb176@aucklanduni.ac.nz>
2018-09-16 17:23:49 +1200

    Added needed packages and code imported from old project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,85 @@
 import * as React from 'react';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import Typography from '@material-ui/core/Typography';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import createStyles from '@material-ui/core/styles/createStyles';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import withRoot from './withRoot';
 
-import logo from './logo.svg';
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      textAlign: 'center',
+      paddingTop: theme.spacing.unit * 20,
+      paddingBottom: theme.spacing.unit * 40,
+      padding: theme.spacing.unit * 50,
+      margin: theme.spacing.unit
+    },
 
-class App extends React.Component {
-  public render() {
+  });
+
+type State = {
+  shown: string;
+  entered: string;
+};
+
+class App extends React.Component<WithStyles<typeof styles>, State> {
+  state = {
+    shown: '',
+    entered: ''
+  };
+
+  handleClick = () => {
+    if (this.state.entered.length > 0) {
+      this.getRhyme();
+    }
+  };
+
+  getRhyme = () => {
+    fetch('https://api.datamuse.com/words?rel_rhy=' + this.state.entered)
+    .then((response: Response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject('Request failed idiot.');
+      }
+    }).then((results: Object) => {
+       this.setState({shown: results[0].word});
+    }).catch((error: Error) => {
+      this.setState({shown: 'A rhyme can\'t be found for ' + this.state.entered + ' please try another word.'});
+    });
+  }
+
+  handleTyping = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({entered: event.target.value});
+  }
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className={this.props.classes.root}>
+        <Typography variant="display1" gutterBottom>
+          Word Rhymer
+        </Typography>
+        <Typography variant="subheading" gutterBottom>
+          MSA 2018
+        </Typography>
+        <Input
+          placeholder="Enter word you wish to rhyme"
+          fullWidth={true}
+          onChange={this.handleTyping}
+        />
+        <Button variant="raised" color="secondary" onClick={this.handleClick}>
+          Rhyme
+        </Button>
+        <Typography variant="body1" gutterBottom>
+          {this.state.shown}
+        </Typography>
       </div>
+
     );
   }
 }
 
-export default App;
+export default withRoot(withStyles(styles)(Index));
+
---
+++ src/withRoot.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+});
+
+function withRoot<P>(Component: React.ComponentType<P>) {
+  function WithRoot(props: P) {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  }
+  return WithRoot;
+}
+
+export default withRoot;
